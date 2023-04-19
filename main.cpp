2c05abcb880828c70ac21127df878312a8ace815
kodai <kodai@MSI>
2023-04-19 15:43:36 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,105 @@
+#include <Windows.h>
+#include <cstdint>
+
+//ウィンドウプロシージャ
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+
+	//メッセージに応じてゲーム固有の処理を行う
+	switch (msg)
+	{
+		//ウィンドウが破棄された
+	case WM_DESTROY:
+		//OSに対して、アプリの終了を伝える
+
+		PostQuitMessage(0);
+
+		return 0;
+
+	}
+
+	//標準のメッセージ処理を行う
+
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+
+}
+
+
+//エントリーポイント
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{	
+
+	WNDCLASS wc{};
+
+	//ウィンドウプロシージャ
+	wc.lpfnWndProc = WindowProc;
+
+	//ウィンドウクラス名
+	wc.lpszClassName = L"CG2WindowClass";
+
+	//インスタンスハンドル
+	wc.hInstance = GetModuleHandle(nullptr);
+
+	//カーソル
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+	//ウィンドウの登録
+	RegisterClass(&wc);
+
+
+	const int32_t kClientWidth = 1280;
+	const int32_t kClientHeight = 720;
+
+	RECT wrc = { 0,0,kClientWidth,kClientHeight };
+
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+
+	//ウィンドウの生成
+
+	HWND hwnd = CreateWindow(
+		wc.lpszClassName, //利用するクラス名
+		L"CG2", //タイトルバーの文字
+		WS_OVERLAPPEDWINDOW, //よく見るウィンドウスタイル
+		CW_USEDEFAULT, //表示X座標
+		CW_USEDEFAULT, //表示Y座標
+		wrc.right - wrc.left, //ウィンドウ横幅
+		wrc.bottom - wrc.top, //ウィンドウ縦幅
+		nullptr, //親ウィンドウハンドル
+		nullptr, //メニューハンドル
+		wc.hInstance, //インスタンスハンドル
+		nullptr //オプション
+		);
+
+
+	//ウィンドウを表示する
+	ShowWindow(hwnd, SW_SHOW);
+
+
+	MSG msg{};
+
+	while (msg.message != WM_QUIT)
+	{
+
+		//Windowにメッセージが来てたら最優先で処理させる
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			//ゲームの処理
+			
+
+		}
+
+	}
+
+
+	//ウィンドウへの文字入力
+	//OutputDebugStringA("Hello,DirectX!\n");
+
+	return 0;
+}
